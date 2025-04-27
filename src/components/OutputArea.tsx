7ec3029d3ff904c521fc84e994ad4ea38faab2d2
ideal3147 <tatu114114@yahoo.co.jp>
2025-04-27 19:32:07 +0900

    feat: create copy button

--- src/components/OutputArea.tsx
+++ src/components/OutputArea.tsx
@@ -1,14 +1,25 @@
+import CopyButton from "@/components/CopyButton";
+
 type OutputAreaProps = {
-    convertedText: string;
-  };
-  
-  const OutputArea = ({ convertedText }: OutputAreaProps) => {
-    return (
-      <div className="w-full p-4 mt-4 border rounded-lg min-h-[150px] text-lg bg-gray-50">
+  convertedText: string;
+};
+
+const OutputArea = ({ convertedText }: OutputAreaProps) => {
+  return (
+    <div className="relative w-full mt-4">
+      {/* テキスト表示部分 */}
+      <div className="p-4 border rounded-lg min-h-[150px] text-lg mb-2 bg-gray-50">
         {convertedText ? convertedText : "ここにけっかがでるよ！"}
       </div>
-    );
-  };
-  
-  export default OutputArea;
-  
+
+      {/* コピーするボタン（右下に絶対配置） */}
+      {convertedText && (
+        <div className="absolute right-2 ">
+          <CopyButton textToCopy={convertedText} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default OutputArea;
