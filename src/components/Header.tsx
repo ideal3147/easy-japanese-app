f00861a51608ae96d9992121e54f9c11dc031847
ideal3147 <tatu114114@yahoo.co.jp>
2025-04-29 19:02:23 +0900

    design: adjust setting button and signout button position

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="w-full max-w-2xl flex justify-between items-center">
       <h1 className="text-2xl font-bold text-indigo-600">✏️かんたんにほんご</h1>
-      <div className="flex flex-col items-end space-y-2">
+      <div className="flex flex-col items-start space-y-2">
         <SettingsButton />
         <SignOutButton />
       </div>
