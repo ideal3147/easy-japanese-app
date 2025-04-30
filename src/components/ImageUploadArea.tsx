8192e81c412e4b2bb68b4391a68b1b23d45c46e8
ideal3147 <tatu114114@yahoo.co.jp>
2025-04-30 11:53:52 +0900

    fix: degration

--- src/components/ImageUploadArea.tsx
+++ src/components/ImageUploadArea.tsx
@@ -73,7 +73,6 @@ export default function ImageUploadArea({ onTextExtracted }: ImageUploadAreaProp
               type="file"
               className="hidden"
               accept="image/*"
-              capture="environment"
               onChange={handleImageUpload}
               ref={fileInputRef}
             />
